fe63917b196a9feba150e5967f8d962071ebfc63
Ruby Alesi <rubyalesi@icloud.com>
2021-01-08 17:50:06 -0800

    adding twitter follow button

--- contact/views.py
+++ contact/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render, get_object_or_404
 
 
 def contact(request):
-    contact = get_object_or_404
     return render(request, 'contact/contact.html', {'contact':contact})
